10dd0198c597cd5122bdde81db3da59f7a477f80
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-09-13 18:44:59 +0530

    Little change in the views

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello, India is a great country!")
+    return HttpResponse("Hello, India is a beautiful country which is known for its diverse culture and heritage!")
 
 def health_check(request):
     return HttpResponse("OK", status=200)
