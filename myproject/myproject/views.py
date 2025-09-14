1054dd1fe4e568a3aa7a5e863bb345c6ae5121f2
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-09-14 18:31:14 +0530

    Changed views

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello, India is a beautiful country which is known for its diverse culture and heritage!")
+    return HttpResponse("My Custom Django App says Hello!")
 
 def health_check(request):
     return HttpResponse("OK, the service is healthy", status=200)
