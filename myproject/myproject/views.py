011136956fea0280af941e7b2cc8513cf1ab7ade
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-10-04 20:03:45 +0530

    New

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("My Custom App litgddsnf !")
+    return HttpResponse("My Custom App lit!")
 
 def health_check(request):
     return HttpResponse("OK, the service is healthy and is telling you to stop me for deploying again and again....!!!", status=200)
